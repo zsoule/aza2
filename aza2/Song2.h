8c37bfc53b635e0b74f7e9c518edaa69c3f9ac9c
Zack <zsoule@college.harvard.edu>
2013-12-05 00:40:26 -0500

    Zack Needs to Take Some Time to Learn Objective C

--- aza2/Song2.h
+++ aza2/Song2.h
@@ -7,6 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface BasicSoundsAppDelegate : NSObject <UIApplicationDelegate, AVAudioPlayerDelegate> {
+	AVAudioPlayer *_backgroundMusicPlayer;
+	BOOL _backgroundMusicPlaying;
+	BOOL _backgroundMusicInterrupted;
+	UInt32 _otherMusicIsPlaying;
+}
+
+
+@end
 
 @interface Song2 : UIViewController
 
@@ -14,3 +26,4 @@
 
 
 @end
+
--- aza2/Song2.m
+++ aza2/Song2.m
@@ -7,12 +7,14 @@
 //
 
 #import "Song2.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface Song2 ()
 
 @end
 
 @implementation Song2
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -27,11 +29,11 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-
-
+    
     CGRect screenSize = [[UIScreen mainScreen] bounds];
     CGFloat screenWidth = screenSize.size.height;
     // have to use height because of screen orientation
+    CGFloat screenHeight = screenSize.size.width;
     
     UILabel *myLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, screenWidth, 240)];
     
@@ -42,6 +44,18 @@
     myLabel.textAlignment = NSTextAlignmentCenter;
     
     [self.view addSubview:myLabel];
+    
+    
+    
+    UIButton *pauseButton = [[UIButton alloc] initWithFrame:CGRectMake((screenWidth-screenWidth/22)/2, screenHeight-screenHeight/12, screenWidth/22, screenHeight/12)];
+
+    
+    [pauseButton setImage:[UIImage imageNamed:@"pause.png"] forState:UIControlStateNormal];
+    [pauseButton addTarget:self action:@selector(pauseTap:) forControlEvents:UIControlEventTouchUpInside];
+     
+     
+    [self.view addSubview:pauseButton];
+    
     
 }
 
@@ -54,8 +68,6 @@
 -(void)pauseTap:(id)sender
 {
     [self.view setBackgroundColor:[UIColor colorWithRed:1 green:0 blue:0 alpha:1]];
-    
-    
     
 }
 
