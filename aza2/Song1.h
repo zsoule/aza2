efdcb9699dd77ff004d16e197061f70907d2d1d3
Zack <zsoule@college.harvard.edu>
2013-12-04 22:05:06 -0500

    For Andrew

---
+++ aza2/Song1.h
@@ -0,0 +1,16 @@
+//
+//  Song1.h
+//  aza2
+//
+//  Created by Zack on 12/4/13.
+//  Copyright (c) 2013 Zack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Song1 : UIViewController
+
+
+-(IBAction)pauseTap:(id)sender;
+
+@end
